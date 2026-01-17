1181c2461e113a2951bbd2fe3a4663ca11857c98
sh1var3ddy <srajamuri@gmail.com>
2026-01-17 15:54:42 -0500

    removed db connection

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ import connectDB from "../src/config/db.js";
 let isConnected = false;
 
 export default async function handler(req, res) {
-    if (!isConnected) {
-        await connectDB();
-        isConnected = true;
-    }
+    // if (!isConnected) {
+    //     await connectDB();
+    //     isConnected = true;
+    // }
 
     return app(req, res);
 }
--- src/app.js
+++ src/app.js
@@ -5,13 +5,17 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.status(200).json({ "message": "Welcome to Kibox API" });
+})
+
 app.use("/api/v1", v1Router);
-app.get("/health/ping",(req,res)=>{
-    res.status(200).json({"message":"pong"});
+app.get("/health/ping", (req, res) => {
+    res.status(200).json({ "message": "pong" });
 })
 
 
