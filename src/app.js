3781570bc1ac13034aa5a24760d3779cd3b49b2f
sh1var3ddy <srajamuri@gmail.com>
2026-03-18 22:33:39 -0400

    Add cors to allow request from frontend

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: "*",
+}));
+app.options("*", cors());
 
 app.get("/", (req, res) => {
     res.status(200).json({ "message": "Welcome to Kibox API" });
